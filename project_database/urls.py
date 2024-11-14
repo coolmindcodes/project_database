e7b83542a3558f0db20690947f34fca9b407a323
Codemind <walter.indeche@proton.me>
2024-11-14 10:32:22 +0300

    Delete customer

--- project_database/urls.py
+++ project_database/urls.py
@@ -22,5 +22,9 @@ from sacco import views
 urlpatterns = [
 
     path('', views.customers, name='customers'),
+
+    path('customers/delete/<int:customer_id>', views.delete_customer, name='delete_customer'),
+
     path('admin/', admin.site.urls),
 ]
+#customers/delete/4
--- sacco/views.py
+++ sacco/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from sacco.models import Customer, Deposit
 
 
@@ -29,3 +29,9 @@ def test(request):
 def customers(request):
     data = Customer.objects.all() # ORM select * from customers
     return render(request, "customers.html", {"customers": data})
+
+
+def delete_customer(request, customer_id):
+    customer = Customer.objects.get(id=customer_id) # select * from customers where id=7
+    customer.delete() # delete from customers where id=7
+    return redirect('customers')
