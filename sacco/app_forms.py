dd94d6e119b5f2f75d4434461faa67272cc0d6b3
Codemind <walter.indeche@proton.me>
2024-11-15 13:05:59 +0300

    Added customer forms

--- sacco/app_forms.py
+++ sacco/app_forms.py
@@ -2,9 +2,18 @@ from django import forms
 
 from sacco.models import Customer
 
-
+GENDER_CHOICES = {"Male": "Male", "Female": "Female"}
 class CustomerForm(forms.ModelForm):
+    gender = forms.ChoiceField(choices=GENDER_CHOICES, widget=forms.RadioSelect)
     class Meta:
         model = Customer
         fields = ['first_name', 'last_name', 'email', 'dob', 'weight', 'gender']
+        widgets = {
+            'dob' : forms.DateInput(attrs={'type': 'date', 'min':'1980-01-01', 'max':'2005-12-31'}),
+            'weight': forms.NumberInput(attrs={'type': 'number', 'min':'35', 'max':'100'})
+        }
 
+
+
+# Update Customer/ Gender radio button
+# Cloning and setting up the virtual env
